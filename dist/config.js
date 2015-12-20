a157bd1f277bd016dd007ddb916f78fa6d4a3ae9
Elton Juan <mikeamos@gmail.com>
2015-12-19 17:43:45 -0800

    stop tracking dist

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var SLACK_ACCESS_TOKEN = exports.SLACK_ACCESS_TOKEN = 'xoxb-15811528817-4WTmBtHjE3SlctJjcSojyszk';
